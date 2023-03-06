cd4cb6d4b3e5261378fcfb5d34da29f81e27c62a
Bin <39841029+xu842251462@users.noreply.github.com>
2023-03-06 16:10:08 -0500

    Add files via upload

---
+++ ec513_lab3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ec513_lab3
+//
+//  Created by Bin Xu on 2/19/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ ec513_lab3/test.c
@@ -0,0 +1,8 @@
+//
+//  test.c
+//  ec513_lab3
+//
+//  Created by Bin Xu on 2/19/23.
+//
+
+#include "test.h"
